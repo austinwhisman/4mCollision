6bd51917fbd067bf1d38d6d9c56094b2e17cb9c8
austinwhisman <27769207+austinwhisman@users.noreply.github.com>
2022-06-09 16:56:42 -0400

    fixed error where server tries to load document obj.

--- src/components/lazyloadscrolleffecthandler.js
+++ src/components/lazyloadscrolleffecthandler.js
@@ -1,7 +1,12 @@
 import React, { useRef, useEffect, useState } from "react"
 
 export const lazyLoadImage = (options) => {
-    const images = document.querySelectorAll('.lazyload');
+    const isBrowser = typeof window !== "undefined"
+
+    if(isBrowser)
+    {
+        var images = document.querySelectorAll('.lazyload');
+    }
 
     const callbackFunction = (entries) => {
        entries.map((entry) => {
@@ -13,9 +18,13 @@ export const lazyLoadImage = (options) => {
         }
     });
     }
-  
+
     const observer = new IntersectionObserver(callbackFunction, options)
-    images.forEach(image => observer.observe(image));
+    if(isBrowser)
+    {
+        images.forEach(image => observer.observe(image));
+    }
+
 }
 
   
